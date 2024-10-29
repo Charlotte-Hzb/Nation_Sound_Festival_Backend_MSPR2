317fd6edc7b2a1af26cfa02c5a514b11a9c908aa
Charlotte-Hzb <161220466+Charlotte-Hzb@users.noreply.github.com>
2024-10-29 21:43:43 +0100

    Update PointsDInteret.ts

--- Backend/src/collections/PointsDInteret.ts
+++ Backend/src/collections/PointsDInteret.ts
@@ -1,34 +1,44 @@
+// Imports the CollectionConfig type from the Payload CMS. 
+// This type is used to define the structure and configuration of a collection within the Payload CMS.
 import { CollectionConfig } from 'payload/types';
 
-const subscribersNewsletter: CollectionConfig = {
-  slug: 'subscribers-newsletters',
+const PointsDInteret: CollectionConfig = {
+  slug: 'points-d-interet',
   // The collection name in the CMS
   labels: {
-    singular: 'Subscriber Newsletter',
-    plural: 'Subscribers Newsletter',
-  },
-  access: {
-    read: () => true, // Allows everyone to read the concerts
-  },
+    singular: 'Point of Interest', 
+    plural: 'Points of Interest',    
+    },
   fields: [
+    // The fields that define the content structure of the collection
     {
-      name: 'email',
-      type: 'email',
+      name: 'name',
+      type: 'text',
       required: true,
-      unique: true,
-      label: 'Email',
     },
     {
-      name: 'createdAt',
-      type: 'date',
-      label: 'Date d\'inscription',
-      admin: {
-        readOnly: true,
-        position: 'sidebar',
-      },
-      defaultValue: () => new Date().toISOString(),
+      name: 'lat',
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'lng',
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'type',
+      type: 'select',
+      options: [
+        { label: 'Accueil', value: 'Accueil' },
+        { label: 'WC', value: 'WC' },
+        { label: 'Scène', value: 'Scène' },
+        { label: 'Buvette', value: 'Buvette' },
+        { label: 'Shop', value: 'Shop' },
+      ],
+      required: true,
     },
   ],
 };
 
-export default subscribersNewsletter;
+export default PointsDInteret;
